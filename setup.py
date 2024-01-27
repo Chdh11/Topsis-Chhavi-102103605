705ccf06dd3f5b53134d50e73554c0fc60f102c5
Chhavi Dhankhar <chhavidhankhar07@gmail.com>
2024-01-27 22:14:14 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis-Chhavi-102103605',         
+  packages = ['Topsis-Chhavi-102103605'],  
+  version = '0.1',     
+  license='MIT',    
+  description = 'This library will help you apply TOPSIS to your datasets for multiple criteria decision making',   
+  author = 'Chhavi Dhankhar',                   
+  author_email = 'chhavidhankhar07@gmail.com',      
+  url = 'https://github.com/Chdh11/Topsis-Chhavi-102103605',   
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',  
+  keywords = ['TOPSIS', 'DECISION-MAKING', 'RANK'],  
+  install_requires=[           
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.11.5'
+  ],
+)
